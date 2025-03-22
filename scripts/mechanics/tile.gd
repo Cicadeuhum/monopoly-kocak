76c62ab48db5671f6ffdf5161bb5bf3417b2802d
2440105186 <rasdy.wibowo@binus.ac.id>
2025-03-22 17:11:17 +0700

    Add asset building

--- scripts/mechanics/tile.gd
+++ scripts/mechanics/tile.gd
@@ -2,22 +2,3 @@ extends Node3D
 class_name Tile
 
 @export var index : int
-@export var asset : Asset
-
-var mesh : MeshInstance3D
-var master : Pawn
-
-func set_master(value: Pawn) -> void:
-	master = value
-	var sm = StandardMaterial3D.new()
-	sm.albedo_color = value.color
-
-	var new_mesh = mesh.mesh.duplicate()
-	
-	var old_material = mesh.mesh.surface_get_material(0)
-	if old_material:
-		var new_material = old_material.duplicate(true)
-		new_material.albedo_color = value.color
-		new_mesh.surface_set_material(0, new_material)
-
-	mesh.mesh = new_mesh
